4ab80a3606f0aefc84dd7c57ab83a31d91fb50f7
mykfrost <okellofrost@gmail.com>
2023-01-11 21:09:09 +0300

    second commits

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
+import './App.css';
+import react from 'react';
+
+export default function App() {
+      const users = [
+{ id: 1, name: 'Nathan', role: 'Web Developer' },
+{ id: 2, name: 'John', role: 'Web Designer' },
+{ id: 3, name: 'Jane', role: 'Team Leader' },
+];
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+       <h1>Users</h1>
+       <p>The current active users in the list</p>
+       <ul>
+        {
+          users.map(function(user){
+            return(<li key={user.id}>Mr {user.name}as the {user.role}</li>)
+          })
+
+        }
+       </ul>
+
     </div>
   );
 }
 
-export default App;
+//export default App;
